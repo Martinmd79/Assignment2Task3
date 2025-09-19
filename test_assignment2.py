eb52934d08f693130916c1a1e7f4e9fc5322c549
Martin Dehghani <matindehghani79@gmail.com>
2025-09-19 21:58:16 +1000

    Update test_assignment2.py

--- test_assignment2.py
+++ test_assignment2.py
@@ -5,30 +5,68 @@ from assignment2 import split_alphabets
 class TestSplitAlphabets(unittest.TestCase):
 
     def setUp(self):
-        """Set up method to run before each test case"""
-        # This runs before each test method
-        # You can initialize any common data here if needed
+        """Optional setup before each test case"""
         pass
 
-    def test_case_1_mixed_characters(self):
-        """Test Case 1: Mixed uppercase and lowercase with duplicates"""
+    def test_case_1_sorting_verification(self):
+        input_chars = ['x', 'C', 'a', 'Z', 'm', 'B']
+        expected_lowercase = ['a', 'm', 'x']
+        expected_uppercase = ['B', 'C', 'Z']
 
-        input_chars = ['C', 'a', 'b', 'Z', 'c', 'Z']
-        expected_lowercase = ['a', 'b', 'c']
-        expected_uppercase = ['C', 'Z']
-
-        # Call the function
         actual_lowercase, actual_uppercase = split_alphabets(input_chars)
-
-        # Assert the results
         self.assertEqual(actual_lowercase, expected_lowercase)
         self.assertEqual(actual_uppercase, expected_uppercase)
 
-    def test_case_2_case_sensivity(self):
-        """Test Case 2: Case sensitivity test"""
+    def test_case_2_case_sensitivity(self):
         input_chars = ['D', 'd']
         expected_lowercase = ['d']
         expected_uppercase = ['D']
+
+        actual_lowercase, actual_uppercase = split_alphabets(input_chars)
+        self.assertEqual(actual_lowercase, expected_lowercase)
+        self.assertEqual(actual_uppercase, expected_uppercase)
+
+    def test_case_3_only_lowercase(self):
+        input_chars = ['b', 'm', 'a', 'w']
+        expected_lowercase = ['a', 'b', 'm', 'w']
+        expected_uppercase = []
+
+        actual_lowercase, actual_uppercase = split_alphabets(input_chars)
+        self.assertEqual(actual_lowercase, expected_lowercase)
+        self.assertEqual(actual_uppercase, expected_uppercase)
+
+    def test_case_4_only_uppercase(self):
+        input_chars = ['Z', 'A', 'N']
+        expected_lowercase = []
+        expected_uppercase = ['A', 'N', 'Z']
+
+        actual_lowercase, actual_uppercase = split_alphabets(input_chars)
+        self.assertEqual(actual_lowercase, expected_lowercase)
+        self.assertEqual(actual_uppercase, expected_uppercase)
+
+    def test_case_5_maximum_boundary(self):
+        input_chars = ['A', 'b', 'C', 'd', 'E', 'f', 'G', 'h', 'I', 'j',
+                       'K', 'l', 'M', 'n', 'O', 'p', 'Q', 'r', 'S', 't']
+        expected_lowercase = ['b', 'd', 'f', 'h', 'j', 'l', 'n', 'p', 'r', 't']
+        expected_uppercase = ['A', 'C', 'E', 'G', 'I', 'K', 'M', 'O', 'Q', 'S']
+
+        actual_lowercase, actual_uppercase = split_alphabets(input_chars)
+        self.assertEqual(actual_lowercase, expected_lowercase)
+        self.assertEqual(actual_uppercase, expected_uppercase)
+
+    def test_case_6_minimal_input(self):
+        input_chars = ['z']
+        expected_lowercase = ['z']
+        expected_uppercase = []
+
+        actual_lowercase, actual_uppercase = split_alphabets(input_chars)
+        self.assertEqual(actual_lowercase, expected_lowercase)
+        self.assertEqual(actual_uppercase, expected_uppercase)
+
+    def test_case_7_identical_repetition(self):
+        input_chars = ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
+        expected_lowercase = []
+        expected_uppercase = ['B']
 
         actual_lowercase, actual_uppercase = split_alphabets(input_chars)
         self.assertEqual(actual_lowercase, expected_lowercase)
